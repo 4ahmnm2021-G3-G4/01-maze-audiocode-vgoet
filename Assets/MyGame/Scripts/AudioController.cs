0646af5c5fd758132d77b040191cf5cecc27df2e
unknown <victoria.goette01@htl-salzburg.ac.at>
2021-04-12 08:32:53 +0200

    Project Settings

--- Assets/MyGame/Scripts/AudioController.cs
+++ Assets/MyGame/Scripts/AudioController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+
+    public string test;
+
+    private int a;
+    private int b;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = "victoria";
+        Debug.Log(a + b);
     }
 
     // Update is called once per frame
